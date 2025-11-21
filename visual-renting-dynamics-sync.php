5d7d89876a3de519a980dad59fbfde3735121b4b
Tom Broucke <tom@tombroucke.be>
2025-11-21 10:05:15 +0100

    🔼 chore(bump): Bump version to 1.13.1

--- visual-renting-dynamics-sync.php
+++ visual-renting-dynamics-sync.php
@@ -11,7 +11,7 @@ use Otomaties\VisualRentingDynamicsSync\Plugin;
 /*
  * Plugin Name:       Visual Renting Dynamics Sync
  * Description:       Syncs Visual Renting Dynamics with WooCommerce
- * Version:           1.13.0
+ * Version:           1.13.1
  * Author:            Tom Broucke
  * Author URI:        https://tombroucke.be
  * License:           GPL v2 or later
